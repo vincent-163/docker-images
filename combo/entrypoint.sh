ed4cc4dfa02cd91c7caa99a58041a6bdb784ac65
vincent163 <vincent.163@outlook.com>
2024-04-14 14:40:39 +0000

    support postgres

--- combo/entrypoint.sh
+++ combo/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p "${MYSQL_DATA_DIR}" "${REDIS_DATA_DIR}" "${SSH_DATA_DIR}" "${MONGODB_DATA_DIR}"
+mkdir -p "${MYSQL_DATA_DIR}" "${REDIS_DATA_DIR}" "${SSH_DATA_DIR}" "${MONGODB_DATA_DIR}" "${POSTGRES_DATA_DIR}"
 
 # Check if MySQL config exists in /data, if not copy the default one
 if [ ! -f "${MYSQL_DATA_DIR}/mysqld.cnf" ]; then
@@ -23,6 +23,16 @@ if [ ! -f "${MONGODB_DATA_DIR}/mongod.conf" ]; then
     cp /etc/mongod.conf ${MONGODB_DATA_DIR}/mongod.conf
 fi
 
+# Check if PostgreSQL config exists in /data, if not copy the default one
+if [ ! -f "${POSTGRES_DATA_DIR}/*/main/postgresql.conf" ]; then
+    # Ensure the PostgreSQL data directory is owned by the postgres user
+    chown -R postgres:postgres ${POSTGRES_DATA_DIR}
+    su - postgres -c "/usr/lib/postgresql/14/bin/initdb -D /data/postgresql/"
+    # Modify pg_hba.conf to allow no-password login
+    echo "local all all trust" > ${POSTGRES_DATA_DIR}/pg_hba.conf
+    echo "host all all all trust" >> ${POSTGRES_DATA_DIR}/pg_hba.conf
+fi
+
 # Start MySQL service to create user and database
 service mysql start
 
@@ -34,6 +44,16 @@ mysql -e "FLUSH PRIVILEGES;"
 
 # Stop MySQL service
 service mysql stop
+
+# Start PostgreSQL service to create user and database
+service postgresql start
+
+# Create PostgreSQL database and user
+su - postgres -c "psql -c \"CREATE DATABASE ${POSTGRES_DB};\""
+su - postgres -c "psql -c \"CREATE USER ${POSTGRES_USER} WITH SUPERUSER PASSWORD '${POSTGRES_PASSWORD}';\""
+
+# Stop PostgreSQL service
+service postgresql stop
 
 # Generate SSH server keys if they do not exist
 for key_type in rsa dsa ecdsa ed25519; do
@@ -57,4 +77,3 @@ chmod 600 /home/user/.ssh/authorized_keys
 
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-
